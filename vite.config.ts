22163cf6759423ba2ca84ed9e2a34c9ace5d70fd
PrayLu <1085353686@qq.com>
2026-07-12 23:45:24 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 import { volcTtsPlugin } from './plugins/volc-tts'
 
 // https://vite.dev/config/
@@ -15,7 +14,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: './',
-    plugins: [inspectAttr(), react(), volcTtsPlugin()],
+    plugins: [react(), volcTtsPlugin()],
     server: {
       port: 3000,
     },
